a1b2ca440055f1bbf87882f1bace6f8bf17a9d23
danielsgriffin <danielsgriffin@gmail.com>
2024-02-02 14:36:00 -0800

    color variation in title

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <a href="/" className="text-4xl font-bold text-center mb-3 mt-2 block">Searchevals</a>
+|      <a href="/" className="text-4xl font-bold text-center mb-3 mt-2 block">Search<span className="text-gray-500">evals</span></a>
       <div className="flex justify-center">
           <p>Search from over <span className='font-bold'>{data.length - 1}</span> public evals!</p>
       </div>
