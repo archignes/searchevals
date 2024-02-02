08e831ed779f66f800763b534dfd7e0948828597
danielsgriffin <danielsgriffin@gmail.com>
2024-02-02 11:55:24 -0800

    fixed location of dynamic-results, closes #5, added responsiveness to search interaction, added close button to dynamic-results

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,14 +11,21 @@ import DataContext from './DataContext'; // Import DataContext
 export default function SearchBar(): JSX.Element {
   const navigate = useNavigate();
   const { data, results, query, setQuery } = useContext(DataContext); // Destructure data from DataContext
+  const [showResults, setShowResults] = useState(true);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
+    setShowResults(true);
   };
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     navigate(`/search?q=${query}`);
+  };
+
+
+  const handleCloseResults = () => {
+    setShowResults(false);
   };
 
 
@@ -34,14 +41,14 @@ export default function SearchBar(): JSX.Element {
   
 
   return (
-    <div className="flex justify-center items-center space-x-2">
-      <div className="search-box-container w-2/3 justify-center items-center flex space-x-2">
-        <form className="flex space-x-2 items-center w-1/2 input-group justify-center search-bar no-link"
-        role="search"
-        aria-label="Website Search"
-        id="search-form"
-        onSubmit={handleSubmit}>
-          <Input type="text" className="form-input search-box" id="search-box"
+    <><div className="flex justify-center items-center space-x-2">
+      <div className="search-box-container w-full md:w-2/3 mx-2 justify-center items-center flex flex-wrap space-x-2">
+        <form className="flex space-x-2 items-center w-full md:w-1/2 input-group justify-center search-bar no-link"
+          role="search"
+          aria-label="Website Search"
+          id="search-form"
+          onSubmit={handleSubmit}>
+          <Input type="text" className="form-input search-box w-full" id="search-bar"
             placeholder="Search..."
             name="q"
             value={query}
@@ -51,22 +58,28 @@ export default function SearchBar(): JSX.Element {
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
           </Button>
-        
         </form>
-        {query && results.length > 0 && query !== new URLSearchParams(window.location.search).get('q') && (
-          <div id="dynamic-results" className="w-1/3 border mt-0 mx-2 p-2 mx-auto bg-white absolute z-50">
-            <ul>
-              {results.map((result: any) => (
-                <li key={result.id}>
-                  <a href={`/card/${result.id}`}>[{result.query}]</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        
-      <Button onClick={goToRandomCard} className="btn mx-auto btn-outline-secondary m-1">Go to a random Eval</Button>
+        <Button onClick={goToRandomCard} className="btn mx-auto btn-outline-secondary m-1 w-full md:w-auto">Go to a random Eval</Button>
+      </div>
+    </div><div className="mt-0 w-full md:w-1/3 ml-0 md:ml-[calc(50%-20rem)] absolute md:mt-0 mt-[-2.6rem]">
+    {query && showResults && results.length > 0 && query !== new URLSearchParams(window.location.search).get('q') && (
+      <div id="dynamic-results" className="border mt-0 p-2 bg-white relative z-50 left-0">
+          <button onClick={handleCloseResults} className="absolute top-0 right-0 p-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+            </svg>
+          </button>
+          <ul>
+          {results.map((result: any) => (
+            <li key={result.id}>
+              <a href={`/card/${result.id}`}>[{result.query}]</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      
+    )}
     </div>
-    </div>
+    </>
   );
 };
