9d4124c6a74f627040697834078c270b3f5e7079
danielsgriffin <danielsgriffin@gmail.com>
2024-02-14 16:45:41 -0800

    typo again

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { GitHubLogoIcon, TwitterLogoIcon, LinkedInLogoIcon } from '@radix-ui/rea
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
-const lastUpdated = "2024-02-15T00:35:51.334Z";
+const lastUpdated = "2024-02-15T00:42:47.202Z";
 
 const Footer: React.FC = () => {
   return (
