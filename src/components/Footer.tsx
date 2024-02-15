dcdaff35dacf5ee2a1140d65c2554f8ebad4f409
danielsgriffin <danielsgriffin@gmail.com>
2024-02-15 12:22:34 -0800

    updated screenshots, re #7

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { GitHubLogoIcon, TwitterLogoIcon, LinkedInLogoIcon } from '@radix-ui/rea
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
-const lastUpdated = "2024-02-15T20:19:42.093Z";
+const lastUpdated = "2024-02-15T20:22:35.000Z";
 
 const Footer: React.FC = () => {
   return (
