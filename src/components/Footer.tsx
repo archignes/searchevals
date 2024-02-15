daedca9ae5679d15fb7892131d462004f578c7ed
danielsgriffin <danielsgriffin@gmail.com>
2024-02-15 11:12:49 -0800

    noting that I've updated my pre-commit so the Footer.tsx is now not only updated but also added to the staging area.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { GitHubLogoIcon, TwitterLogoIcon, LinkedInLogoIcon } from '@radix-ui/rea
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
-const lastUpdated = "2024-02-15T19:05:06.489Z";
+const lastUpdated = "2024-02-15T19:12:49.538Z";
 
 const Footer: React.FC = () => {
   return (
