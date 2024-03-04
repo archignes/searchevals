ae77e7b41ae87df37024fd4970fff3841bccd590
danielsgriffin <danielsgriffin@gmail.com>
2024-03-04 11:23:21 -0800

    small update to README re Next.js; added eval

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { GitHubLogoIcon, TwitterLogoIcon, LinkedInLogoIcon } from '@radix-ui/rea
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
-const lastUpdated = "2024-02-29T14:52:56.650Z";
+const lastUpdated = "2024-03-04T19:23:21.757Z";
 
 const Footer: React.FC = () => {
   return (
